2ebee31ba2aeb323e9434fef68690d55f3010e59
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2024-04-16 21:55:18 +0900

    first commit

---
+++ src/i18n-config.ts
@@ -0,0 +1,8 @@
+// multilanguage setting are done based on this page:https://github.com/vercel/next.js/tree/canary/examples/app-dir-i18n-routing
+
+export const i18n = {
+  defaultLocale: "ja",
+  locales: ["ja", "ko"],
+} as const;
+
+export type Locale = (typeof i18n)["locales"][number];
