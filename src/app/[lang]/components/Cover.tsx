f56e664df573d6475842fc8a0ac432f8bedede36
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2024-05-03 22:14:30 +0900

    cover props added

--- src/app/[lang]/components/Cover.tsx
+++ src/app/[lang]/components/Cover.tsx
@@ -6,36 +6,38 @@ import { type Locale } from "../../../i18n-config";
 import Layout from "./layouts/Layout";
 import coverImage from "/public/images/Currency-rafiki.png";
 
-interface Dictionary {
-  Product: string;
-  Features: string;
-  Team: string;
-  JoinUs: string;
-}
-
 interface CoverProps {
-  cover_props: Dictionary;
-  lang: Locale;
+  cover_props: {
+    headline1: string;
+    headline2: string;
+    headline3: string;
+    comment1: string;
+    comment2: string;
+    comment3: string;
+    comment4: string;
+    button1: string;
+    button2: string;
+  };
 }
 
-const Cover = ({ cover_props, lang }: CoverProps) => {
+const Cover = ({ cover_props }: CoverProps) => {
   return (
     <Layout backgroundColor="bg-white">
       <div className="flex flex-wrap items-center -mx-3 xl:px-16 pt-8 md:pt-16 lg:pt-0">
         <div className="w-full lg:w-1/2 px-3">
           <div className="max-w-lg lg:max-w-md mx-auto lg:mx-0 mb-8 text-center lg:text-left">
             <h2 className="text-3xl lg:text-5xl mb-4 font-bold font-heading wow animate__animated animate__fadeIn">
-              Exclusive Agency For
-              <span className="text-blue-500"> Technology </span>
-              Provide Solution
+              {cover_props.headline1}
+              <span className="text-blue-500">{cover_props.headline2}</span>
+              {cover_props.headline3}
             </h2>
             <div className="text-blueGray-400 leading-relaxed wow animate__animated animate__fadeIn">
-              We are
-              <strong className="text-blue-500"> Monst</strong>, a Creative
-              Design
+              {cover_props.comment1}
+              <strong className="text-blue-500">{cover_props.comment2}</strong>
+              {cover_props.comment3}
             </div>
             <p className="text-blueGray-400 leading-relaxed wow animate__animated animate__fadeIn mt-3 text-sm">
-              Helping you maximize operations management with digitization
+              {cover_props.comment4}
             </p>
           </div>
           <div className="text-center lg:text-left">
@@ -43,14 +45,14 @@ const Cover = ({ cover_props, lang }: CoverProps) => {
               className="tracking-wide hover-up-2 block sm:inline-block py-4 px-8 mb-4 sm:mb-0 sm:mr-3 text-xs text-white text-center font-semibold leading-none bg-blue-500 hover:bg-blue-700 rounded wow animate__animated animate__fadeIn"
               href="#Features"
             >
-              Key Features
+              {cover_props.button1}
             </Link>
             <Link
               className="block sm:inline-block hover-up-2 py-4 px-8 text-xs text-blueGray-500 hover:text-blueGray-600 text-center font-semibold leading-none bg-white border border-blueGray-200 hover:border-blueGray-300 rounded wow animate__animated animate__fadeIn"
               data-wow-delay=".3s"
               href="#Team"
             >
-              Who We Are?
+              {cover_props.button2}
             </Link>
           </div>
         </div>
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -18,7 +18,7 @@ export default async function IndexPage({
   return (
     <main>
       <Header header_props={dictionary.header_props} lang={lang} />
-      <Cover cover_props={dictionary.cover_props} lang={lang} />
+      <Cover cover_props={dictionary.cover_props} />
       <Product product_props={dictionary.product_props} lang={lang} />
       <Features features_props={dictionary.features_props} />
       <Team team_props={dictionary.team_props} lang={lang} />
